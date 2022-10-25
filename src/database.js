604de59e6f83e3a615a1094ae84392be53b4950d
EzPlays <ezapata113@misena.edu.co>
2022-10-25 15:14:49 -0500

    db local y host conmentado

--- src/database.js
+++ src/database.js
@@ -1,12 +1,20 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-  host: 'i1rf7b.stackhero-network.com',
+  host: 'localhost',
   user: 'root',
-  password: 'IFhkS7pD1QOvZ7fRZXuHDw5mPyDf0PB0',
+  password: '',
   database: 'tienda_virtual',
   multipleStatements: true
 });
+
+// const mysqlConnection = mysql.createConnection({
+//   host: 'i1rf7b.stackhero-network.com',
+//   user: 'root',
+//   password: 'IFhkS7pD1QOvZ7fRZXuHDw5mPyDf0PB0',
+//   database: 'tienda_virtual',
+//   multipleStatements: true
+// });
 
 mysqlConnection.connect(function (err) {
   if (err) {
